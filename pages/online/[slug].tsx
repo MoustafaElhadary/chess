b9c4b24679e07ea538a5d536de8fe9aa893f8ac4
moustafaElhadary <moustafaElhadary96@gmail.com>
2022-01-07 13:44:44 -0500

    gaame join

--- pages/online/[slug].tsx
+++ pages/online/[slug].tsx
@@ -5,7 +5,7 @@ import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
-import useSWR, { useSWRConfig } from "swr";
+import useSWR, { KeyedMutator } from "swr";
 import { GameEndpointResponse } from "types";
 
 const Online: NextPage = () => {
@@ -13,27 +13,40 @@ const Online: NextPage = () => {
   const user = auth?.user;
   const router = useRouter();
   const { slug } = router.query;
-  const { mutate } = useSWRConfig();
-  const { data: game }: { data?: GameEndpointResponse } = useSWR(
+  const {
+    data: game,
+    mutate,
+  }: { data?: GameEndpointResponse; mutate: KeyedMutator<any> } = useSWR(
     `/api/game/${slug}`
   );
-
-  const joinGame = async () => {
-    const { data, error } = await mutate("/api/game/join", {
-      userId: user?.id,
-      slug,
-    });
-    console.log({ data, user: user });
-    return data;
-  };
 
   useEffect(() => {
     initGame();
   }, []);
 
   return (
-    <div className="min-h-screen justify-center align-middle flex">
-      {game && slug && !game.player2 && <> Join game</>}
+    <div className="min-h-screen justify-center align-middle flex flex-col">
+      {game && slug && !game.player2 && (
+        <button
+          type="button"
+          className="items-center m-auto px-6 py-3 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          onClick={async () => {
+            await fetch("/api/game/join", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                userId: user?.id,
+                slug,
+              }),
+            });
+            mutate();
+          }}
+        >
+          Join Game
+        </button>
+      )}
       <Board isBlack={user?.id === game?.player2_id} />
     </div>
   );
